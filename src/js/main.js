e827bb46d79db63aaad7436a91a46bf9c7b10ea0
pomme <apple.cjcfardel@yahoo.com>
2015-02-23 11:57:21 +0000

    add sticky element(s) on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,45 @@
 define([
-    'chart/chart',
-    'pollchart/pollchart',
-    'text!templates/appTemplate.html'
+  'underscore',
+  'chart/chart',
+  'pollchart/pollchart',
+  'text!templates/appTemplate.html'
 ], function(
-    chartView,
-    pollChart,
-    templateHTML
+  underscore,
+  chartView,
+  pollChart,
+  templateHTML
 ) {
-   'use strict';
+  'use strict';
 
-    function init(el, context, config, mediator) {
-        // DEBUG: What we get given on boot
-        console.log(el, context, config, mediator);
-        console.log(pollChart);
-        console.log(templateHTML);
-        el.innerHTML = templateHTML;
+  function init(el, context, config, mediator) {
+    // DEBUG: What we get given on boot
+    //console.log(el, context, config, mediator);
+    //console.log(pollChart);
+    //console.log(templateHTML);
+    el.innerHTML = templateHTML;
+    stickElementOnScroll();
 
+    chartView.render(el.querySelector('#chart'));
+    pollChart.render(el.querySelector('#pollchart'));
+  }
 
-
-        chartView.render(el.querySelector('#chart'));
-        pollChart.render(el.querySelector('#pollchart'));
+  /* stick element (time and party labels) to top on scroll */
+  function stickElementOnScroll() {
+    var el = document.querySelector("#stickyRow"),
+        offset = el.offsetTop;
+    
+    window.onscroll = _.throttle(stickIfNeeded, 100);
+    
+    function stickIfNeeded() {
+      if (offset <= window.pageYOffset) {
+        el.classList.add("l-stick");
+      } else {
+        el.classList.remove("l-stick");
+      }
     }
+  }
 
-    return {
-        init: init
-    };
+  return {
+    init: init
+  };
 });
