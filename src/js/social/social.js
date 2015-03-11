e301681c9076a4c8b93f977742e932143ab887c5
czapponi <littleark@gmail.com>
2015-03-11 12:14:23 +0000

    using one social function accross all pieces

--- src/js/social/social.js
+++ src/js/social/social.js
@@ -1,36 +1,58 @@
 define([
-    'd3'
+    
 ], function(
-    d3
+    
 ) {
 
+  'use strict';
+
+  function shareInteractive(){
+    var shareButtons = document.querySelectorAll('.btns-share button');
     
-    var twitterBaseUrl = "https://twitter.com/home?status=";
-    var facebookBaseUrl = "https://www.facebook.com/dialog/feed?display=popup&app_id=741666719251986&link=";
-    var sharemessage = this.mainVideo.couple + " were both given Google Glass and sent on a date. What happens next? #WatchMeDate ";
-    var network = $(e.currentTarget).attr('data-source'); //make sure to add the network (pinterest,twitter,etc) as a classname to the target
-    var shareWindow = "";
-    var queryString = "?date="+this.mainVideo.coupleid;
-    var coupleImage = "{{assets}}/imgs/dates/" + this.mainVideo.coupleid + '_1260.jpg';
-    var guardianUrl = "http://www.theguardian.com/lifeandstyle/ng-interactive/2015/feb/12/watch-me-date" + queryString;
+    for (var i = 0; i < shareButtons.length; i++) {
+      shareButtons[i].addEventListener('click',openShareWindow);
+    };
 
+    function openShareWindow(e){
+      var shareWindow = "";
+      var twitterBaseUrl = "https://twitter.com/home?status=";
+      var facebookBaseUrl = "https://www.facebook.com/dialog/feed?display=popup&app_id=741666719251986&link=";
+      var network = e.currentTarget.getAttribute('data-source'); 
+      
 
-    if(network === "twitter"){
-        shareWindow = 
-            twitterBaseUrl + 
-            encodeURIComponent(sharemessage) + 
-            "%20" + 
-            encodeURIComponent(guardianUrl)
-        
-    }else if(network === "facebook"){
-        shareWindow = 
-            facebookBaseUrl + 
-            encodeURIComponent(guardianUrl) + 
-            "&picture=" + 
-            encodeURIComponent(coupleImage) + 
-            "&redirect_uri=http://www.theguardian.com";
+      var currentView = e.currentTarget.getAttribute('data-view');
+      var defaultSharemessage = "The Guardian poll projection. ";
+      
+      var sharemessage=document.querySelector("a[name=default]");
+      if(currentView) {
+        sharemessage = document.querySelector("a[name="+currentView+"]");
+      }
+
+      sharemessage=sharemessage.getAttribute("data-status");
+
+      var shareImage = "";
+      var guardianUrl = "http://gu.com/p/464t6"+(currentView?("#"+currentView):"");
+
+       
+      if(network === "twitter"){
+          shareWindow = 
+              twitterBaseUrl + 
+              encodeURIComponent(sharemessage) + 
+              "%20" + 
+              (currentView?encodeURIComponent(guardianUrl):"")
+          
+      }else if(network === "facebook"){
+          shareWindow = 
+              facebookBaseUrl + 
+              encodeURIComponent(guardianUrl) + 
+              "&picture=" + 
+              encodeURIComponent(shareImage) + 
+              "&redirect_uri=http://www.theguardian.com";
+      }
+      window.open(shareWindow, network + "share", "width=640,height=320");
     }
-    window.open(shareWindow, network + "share", "width=640,height=320");
+  }
 
+  return shareInteractive;
 
 });
