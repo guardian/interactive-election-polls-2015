327e5cbfc9b8a80a3073be31e0deb752738fb692
czapponi <littleark@gmail.com>
2015-03-11 12:19:04 +0000

    using social.js in main and removing shareInteractive function

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ define([
   'page/page',
   'seatscharts/commons',
   'pollchart/pollchart',
+  'social/social',
   'text!templates/appTemplate.html'
 ], function(
   d3,
@@ -11,6 +12,7 @@ define([
   pageView,
   commonsChart,
   pollChart,
+  shareInteractive,
   templateHTML
 ) {
   'use strict';
@@ -40,52 +42,7 @@ define([
     //history.replaceState(null,null,url);
   }
 
-  function shareInteractive(){
-    var shareButtons = document.querySelectorAll('.btns-share button');
-    
-    for (var i = 0; i < shareButtons.length; i++) {
-      shareButtons[i].addEventListener('click',openShareWindow);
-    };
-
-    function openShareWindow(e){
-      var shareWindow = "";
-      var twitterBaseUrl = "https://twitter.com/home?status=";
-      var facebookBaseUrl = "https://www.facebook.com/dialog/feed?display=popup&app_id=741666719251986&link=";
-      var network = e.currentTarget.getAttribute('data-source'); 
-      
-
-      var currentView = e.currentTarget.getAttribute('data-view');
-      var defaultSharemessage = "The Guardian poll projection. ";
-      
-      var sharemessage=document.querySelector("a[name=default]");
-      if(currentView) {
-        sharemessage = document.querySelector("a[name="+currentView+"]");
-      }
-
-      sharemessage=sharemessage.getAttribute("data-status");
-
-      var shareImage = "";
-      var guardianUrl = "http://gu.com/p/464t6"+(currentView?("#"+currentView):"");
-
-       
-      if(network === "twitter"){
-          shareWindow = 
-              twitterBaseUrl + 
-              encodeURIComponent(sharemessage) + 
-              "%20" + 
-              (currentView?encodeURIComponent(guardianUrl):"")
-          
-      }else if(network === "facebook"){
-          shareWindow = 
-              facebookBaseUrl + 
-              encodeURIComponent(guardianUrl) + 
-              "&picture=" + 
-              encodeURIComponent(shareImage) + 
-              "&redirect_uri=http://www.theguardian.com";
-      }
-      window.open(shareWindow, network + "share", "width=640,height=320");
-    }
-  }
+  
 
 
   function init(el, context, config, mediator) {
