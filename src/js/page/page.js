860292f8e0848f605933c911540219f98d82855f
czapponi <littleark@gmail.com>
2015-03-13 15:11:45 +0000

    lastupdate from spreadsheet

--- src/js/page/page.js
+++ src/js/page/page.js
@@ -8,14 +8,17 @@ define([
     function render(data, latest, updated) {
         
 
-        var format=d3.time.format("%d/%m/%Y")
+        var format=d3.time.format("%d/%m/%Y"),
+            datetime_format=d3.time.format("%d/%m/%Y %H:%M:%S")
 
         var today = new Date(),
             electionDay = new Date(2015,4,7),
             diff = d3.time.format("%j")(electionDay) - d3.time.format("%j")(today);
 
+        var lastupdate_date=datetime_format.parse(latest.currentdate+" "+latest.currenttime);            
         d3.select("#lastUpdate time")
-            .text(d3.time.format("%b %d %Y %H:%m")(new Date(updated))+" GMT")
+            .text(d3.time.format("%b %d %Y %H:%m")(lastupdate_date)+" "+(!lastupdate_date.getTimezoneOffset()?"GMT":"BST"))
+
 
         data.forEach(function(d){
             d3.select("#title_"+d.component).text(d["title"])
